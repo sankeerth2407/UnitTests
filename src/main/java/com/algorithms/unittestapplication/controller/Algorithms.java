3b8e60fe866fb05a7fdc6e2cbaa02b4208178d7e
sankeerth <sankeerth.cholleti@gmail.com>
2022-11-08 14:46:05 -0600

    fixed tests

--- src/main/java/com/algorithms/unittestapplication/controller/Algorithms.java
+++ src/main/java/com/algorithms/unittestapplication/controller/Algorithms.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,7 +26,7 @@ public class Algorithms {
     }
 
     @GetMapping(value = "/isEven")
-    public Boolean iseven(Integer number){
+    public Boolean iseven(@RequestParam Integer number){
         return number % 2 == 0;
     }
 }
